112caddae07e050286f3601c0850460aa091d4e8
BushiGYM <bushidavi@gmail.com>
2025-03-09 22:48:07 -0300

    Desafio mestre Super Trunfo em C
    Fase final da primeira parte do desafio mestre do super trunfo em C.

--- supertrunfo.c
+++ supertrunfo.c
@@ -5,12 +5,12 @@ int main(){
     char estado1;
     char codigo1[5] , cidade1[51];
     int popul1 , turismo1;
-    float area1 , pib1, PPC1, DP1;
+    float area1 , pib1, PPC1, DP1, superpoder1;
 //variaveis da carta 2
     char estado2;
     char codigo2[5] , cidade2[51];
     int popul2 , turismo2;
-    float area2 , pib2, PPC2, DP2;
+    float area2 , pib2, PPC2, DP2, superpoder2;
 
 
     
@@ -80,7 +80,7 @@ int main(){
     printf("PIB: R$%.2f \n", pib1);
     printf("Pontos turisticos: %d \n", turismo1);
     printf("Densidade Populacional: %.2f hab/km²\n" , DP1);
-    printf("PIB per capta: R$%.2f \n", PPC1);
+    printf("PIB per capta: R$%.2f Bilhões\n", PPC1);
     printf("------------------ \n");
 // apresentacao da carta 2
     printf("-----CARTA 2-----\n");
@@ -92,8 +92,21 @@ int main(){
     printf("PIB: R$%.2f \n", pib2);
     printf("Pontos turisticos: %d \n", turismo2);
     printf("Densidade Populacional: %.2f hab/km²\n" , DP2);
-    printf("PIB per capta: R$%.2f \n", PPC2);
-    printf("-----------------");
+    printf("PIB per capta: R$%.2f Bilhões\n", PPC2);
+        superpoder2 = (float) popul2 + area2 + pib2 + (float)turismo2 + PPC2 + DP2;
+    printf("Super-poder: %.2f\n", superpoder2);
+    printf("-----------------\n");
+
+// prova da força das cartas
+
+        popul1 > popul2 ? printf("População: A Carta 1 ganhou!\n") : printf("População: A carta 2 ganhou!\n");
+        area1 > area2 ? printf("Area: A Carta 1 ganhou!\n") : printf("Area: A carta 2 ganhou!\n");
+        pib1 > pib2 ? printf("PIB: A Carta 1 ganhou!\n") : printf("PIB: A carta 2 ganhou!\n");
+        turismo1 > turismo2 ? printf("Pontos turísticos: A Carta 1 ganhou!\n") : printf("Pontos turísticos: A carta 2 ganhou!\n");
+        DP1 < DP2 ? printf("Densidade populacional: A Carta 1 ganhou!\n") : printf("Densidade populacional: A carta 2 ganhou!\n");
+        PPC1 > PPC2 ? printf("PIB per capta: A Carta 1 ganhou!\n") : printf("PIB per capta: A carta 2 ganhou!\n");
+        superpoder1> superpoder2 ? printf("Super-poder: A carta 1 ganhou!\n") : printf("Super-poder: A carta 2 ganhou!\n");
+
 
     return 0;
 
